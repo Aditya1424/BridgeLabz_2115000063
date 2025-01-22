546c1060edb4870ad8e4cb0f89187470ffdac610
Aditya Gupta <ag7023587@gmail.com>
2025-01-22 12:00:58 +0530

    Program 1 added

---
+++ age.java
@@ -0,0 +1,10 @@
+public class age{
+	public static void main(String []args){
+		int curr_year =2024;
+		int birth_year = 2000;
+		
+		int age = curr_year- birth_year;
+		
+		System.out.println("Age is: "+age);
+	}
+}
