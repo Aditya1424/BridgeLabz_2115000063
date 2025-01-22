89efa62a5f446cab1fe3227fde95eeb0788e4913
Aditya Gupta <ag7023587@gmail.com>
2025-01-22 19:28:42 +0530

    Program 27 added

---
+++ weight.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class weight{
+	public static void main(String []args){
+		Scanner adi= new Scanner(System.in);
+		
+		double	pounds= adi.nextDouble();
+		
+		double kg= pounds/2.2;
+		
+		System.out.println(kg);
+	}
+}
