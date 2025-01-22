bd227468b770690b5166ecbbaff755e4d276c767
Aditya Gupta <ag7023587@gmail.com>
2025-01-22 15:15:31 +0530

    Program 12 added

---
+++ square_perimeter.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class square_perimeter {
+    public static void main(String[] args) {
+        Scanner adi = new Scanner(System.in);
+
+        
+        double perimeter = adi.nextDouble();
+
+        double side = perimeter/4;
+
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+
+        
+    }
+}
